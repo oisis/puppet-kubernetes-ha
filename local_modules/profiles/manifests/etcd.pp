9c3e0ec9019a1395176fedb9b7daaa36b393d019
OiSiS <oisis@o2.pl>
2016-08-26 12:11:45 +0200

    Move facts to module directory

--- local_modules/profiles/manifests/etcd.pp
+++ local_modules/profiles/manifests/etcd.pp
@@ -13,13 +13,6 @@ class profiles::etcd (
   $cluster_network,
 ){
   include ::etcd
-  file { '/etc/facter/facts.d/etcd_entry.sh':
-    ensure  => file,
-    content => template('profiles/etcd/etcd_entry.sh.erb'),
-    group   => 'root',
-    owner   => 'root',
-    mode    => 0755,
-  }
   # Add etcd entry if does not exist
   if $::etcd_health == 'ok' and $::etcd_key == "notfound" {
     etcd_key { "$cluster_domain":
