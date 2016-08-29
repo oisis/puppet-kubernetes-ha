a9bbde24f8d0aaf7d16e17165f395693471dd031
OiSiS <oisis@o2.pl>
2016-08-29 22:21:56 +0200

    Add class for etcd_key creation

---
+++ local_modules/profiles/manifests/etcd_key.pp
@@ -0,0 +1,11 @@
+# -*- mode: puppet -*-
+# vi: set ft=puppet
+#
+# === Author
+# OiSiS (https://github.com/oisis/)
+#
+class profiles::etcd_key (){
+  exec{ 'create_etcd_entry':
+    command   => "/bin/curl -X PUT -d \"value={\\\"Network\\\":\\\"10.1.0.0/16\\\",\\\"Backend\\\":{\\\"Type\\\":\\\"vxlan\\\",\\\"VNI\\\":1}}\" \"http://etcd1.example.com:2379/v2/keys/example.com/network/config\""
+  }
+}
