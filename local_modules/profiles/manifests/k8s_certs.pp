ed5ddde98b1235b334d6b3e0a2a67712f03c36ba
OiSiS <oisis@o2.pl>
2016-08-31 00:28:47 +0200

    Copy and run script to server

--- local_modules/profiles/manifests/k8s_certs.pp
+++ local_modules/profiles/manifests/k8s_certs.pp
@@ -5,5 +5,15 @@
 # OiSiS (https://github.com/oisis/)
 #
 class profiles::k8s_certs () {
-  include ::k8s_certs
+  file{ '/root/generate_k8s_certs.sh':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0700',
+    content => template("${module_name}/k8s_certs/k8s_generate_certs.sh.erb"),
+  } ->
+  exec{ 'generate_k8s_certs':
+    command => "/bin/sh /root/generate_k8s_certs.sh",
+    path    => '/bin:/sbin:/usr/bin:/usr/sbin',
+  }
 }
