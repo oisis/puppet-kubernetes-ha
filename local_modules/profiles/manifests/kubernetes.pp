a0927333153f1d392c912d145e1c7d7e80a45bce
OiSiS <oisis@o2.pl>
2016-09-07 23:05:49 +0200

    Define Cockpit installation on kubernetes servers

--- local_modules/profiles/manifests/kubernetes.pp
+++ local_modules/profiles/manifests/kubernetes.pp
@@ -4,6 +4,16 @@
 # === Author
 # OiSiS (https://github.com/oisis/)
 #
-class profiles::kubernetes (){
+class profiles::kubernetes (
+  $k8s_gui = hiera('progiles::kubernetes::k8s_gui'),
+){
   include ::kubernetes
+
+  if $k8s_gui == 'true'{
+    create_resources(package, hiera('k8s_frontend', {}))
+    service { 'cockpit':
+      ensure => 'running',
+      require => Package['cockpit'],
+    }
+  }
 }
