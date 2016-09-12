11e7c80ec98d6e48ead52f87f9697ca31be37249
OiSiS <oisis@o2.pl>
2016-09-12 23:08:25 +0200

    Change domain for etcd_prefix to cluster.local

--- local_modules/profiles/manifests/etcd_key.pp
+++ local_modules/profiles/manifests/etcd_key.pp
@@ -6,6 +6,6 @@
 #
 class profiles::etcd_key (){
   exec{ 'create_etcd_entry':
-    command   => "/bin/curl -X PUT -d \"value={\\\"Network\\\":\\\"10.1.0.0/16\\\",\\\"Backend\\\":{\\\"Type\\\":\\\"vxlan\\\",\\\"VNI\\\":1}}\" \"http://etcd1.example.com:2379/v2/keys/example.com/network/config\""
+    command   => "/bin/curl -X PUT -d \"value={\\\"Network\\\":\\\"10.1.0.0/16\\\",\\\"Backend\\\":{\\\"Type\\\":\\\"vxlan\\\",\\\"VNI\\\":1}}\" \"http://etcd1.example.com:2379/v2/keys/cluster.local/network/config\""
   }
 }
