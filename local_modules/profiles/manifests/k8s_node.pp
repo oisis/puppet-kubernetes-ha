dc9c05fbcb4fe8024d8920c95c94d93956fdcab4
OiSiS <oisis@o2.pl>
2016-09-24 19:32:22 +0200

    Add common GlusterFS settings and puppet modules

--- local_modules/profiles/manifests/k8s_node.pp
+++ local_modules/profiles/manifests/k8s_node.pp
@@ -8,8 +8,10 @@ class profiles::k8s_node (){
   include ::profiles::flannel
   include ::profiles::docker
   include ::profiles::kubernetes
+  include ::profiles::k8s_node::glusterfs
 
   Class['::profiles::docker']   ->
   Class['::profiles::flannel']  ->
+  Class['::profiles::k8s_node::glusterfs'] ->
   Class['::profiles::kubernetes']
 }
