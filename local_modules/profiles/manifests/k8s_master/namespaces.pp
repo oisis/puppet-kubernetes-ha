a409ba27c0dc78dc98ce1a5681901b872d114538
OiSiS <oisis@o2.pl>
2016-09-24 22:52:09 +0200

    Swit hiera_array to hiera because validation

--- local_modules/profiles/manifests/k8s_master/namespaces.pp
+++ local_modules/profiles/manifests/k8s_master/namespaces.pp
@@ -5,7 +5,7 @@
 # OiSiS (https://github.com/oisis/)
 #
 class profiles::k8s_master::namespaces (
-  $k8s_namespaces = hiera_array('profiles::kubernetes::k8s_namespaces'),
+  $k8s_namespaces         = hiera('profiles::kubernetes::k8s_namespaces'),
   $k8s_create_namespaces  = hiera('profiles::kubernetes::k8s_create_namespaces'),
 ){
   validate_array($k8s_namespaces)
